c0e8f100139e7b836f1668729df964dfa802fdd2
JosephBeckett <joseph.beckett1999@gmail.com>
2019-11-17 19:37:29 +0000

    Recent search display changes

--- carsearch.php
+++ carsearch.php
@@ -22,23 +22,10 @@ include("session.php");
   include "navbar.php";
   include "savecar.php";
   ?>
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
       <div class='car-search-box'>
-        <div class=" btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <?php
-          $recentsearches = "SELECT * FROM recentsearch WHERE userID = $userID ORDER BY timeSearched DESC LIMIT 5";
-          $rsq = mysqli_query($conn, $recentsearches);
-          $rsqr = mysqli_num_rows($rsq);
-          if ($rsqr > 0) {
-            while ($rsqrow  = mysqli_fetch_assoc($rsq)) {
-              echo'
-              <button type="button" class="btn btn-secondary">'. $rsqrow['searchQuery'] .'</button>
-              ';
-            }
-          }
-          ?>
-        </div>
+
         <form class="car-search-form" action="carsearch.php" method="POST">
           <input class="car-search-input" type="text" name="search" placeholder="Search for a car">
           <button class="car-search-button" type="submit" name="submit-search">Search</button>
@@ -47,74 +34,89 @@ include("session.php");
       </div>
     </div>
     <div class="row">
-
-
-
-    </div>
-    <div class="row">
-      <?php
-      if (!isset($_POST['submit-search']) && !isset($_POST['favourite-search'])) {
-        $carlist = 'SELECT * FROM Cars';
-        $carresults = mysqli_query($conn, $carlist);
-        $queryResult = mysqli_num_rows($carresults);
-        if ($queryResult > 0) {
-          while ($row = mysqli_fetch_assoc($carresults)) {
-            include("cardisplayform.php");
+      <div class="col-2 mb-2">
+        <div class="list-group">
+          <?php
+          $recentsearches = "SELECT * FROM recentsearch WHERE userID = $userID ORDER BY timeSearched DESC LIMIT 10";
+          $rsq = mysqli_query($conn, $recentsearches);
+          $rsqr = mysqli_num_rows($rsq);
+          if ($rsqr > 0) {
+            while ($rsqrow  = mysqli_fetch_assoc($rsq)) {
+              echo'
+              <a href="#" class="list-group-item list-group-item-action">'. $rsqrow['searchQuery'] .'</a>
+              ';
+            }
           }
-        }
-      } else if (isset($_POST['submit-search'])) {
-        $search = mysqli_real_escape_string($conn, $_POST['search']);
-        if ($search == "") {
-          header("location: carsearch.php");
-        } else {
-          // check if query is already saved
-          $recentsql = "SELECT * FROM recentsearch WHERE userID='$userID' AND searchQuery='$search';";
-          $searchresults = mysqli_query($conn, $recentsql);
-          $resultCheck = mysqli_num_rows($searchresults);
-          if ($resultCheck > 0) {
-            // increment the frequency column
-            if ($searchrow = mysqli_fetch_assoc($searchresults)) {
-              $amount = $searchrow['amountSearched'] + 1;
-              $searchtime = date("Y/m/d h:i:sa");
-              $update_sql = "UPDATE recentsearch SET amountSearched='$amount', timeSearched='$searchtime' WHERE searchQuery='$search' AND userID='$userID';";
-              mysqli_query($conn, $update_sql);
+          ?>
+        </div>
+      </div>
+      <div class="col-10">
+        <div class="row">
+          <?php
+          if (!isset($_POST['submit-search']) && !isset($_POST['favourite-search'])) {
+            $carlist = 'SELECT * FROM Cars';
+            $carresults = mysqli_query($conn, $carlist);
+            $queryResult = mysqli_num_rows($carresults);
+            if ($queryResult > 0) {
+              while ($row = mysqli_fetch_assoc($carresults)) {
+                include("cardisplayform.php");
+              }
+            }
+          } else if (isset($_POST['submit-search'])) {
+            $search = mysqli_real_escape_string($conn, $_POST['search']);
+            if ($search == "") {
+              header("location: carsearch.php");
             } else {
-              header("location: carsearch.php?amount".$amount);
+              // check if query is already saved
+              $recentsql = "SELECT * FROM recentsearch WHERE userID='$userID' AND searchQuery='$search';";
+              $searchresults = mysqli_query($conn, $recentsql);
+              $resultCheck = mysqli_num_rows($searchresults);
+              if ($resultCheck > 0) {
+                // increment the frequency column
+                if ($searchrow = mysqli_fetch_assoc($searchresults)) {
+                  $amount = $searchrow['amountSearched'] + 1;
+                  $searchtime = date("Y/m/d h:i:sa");
+                  $update_sql = "UPDATE recentsearch SET amountSearched='$amount', timeSearched='$searchtime' WHERE searchQuery='$search' AND userID='$userID';";
+                  mysqli_query($conn, $update_sql);
+                } else {
+                  header("location: carsearch.php?amount".$amount);
+                }
+              } else {
+                // add query to database
+                $insert_sql = "INSERT INTO recentsearch (userID, searchQuery, amountSearched) VALUES ('$userID', '$search', '1');";
+                mysqli_query($conn, $insert_sql);
+              }
+              $sql = "SELECT * FROM Cars WHERE Make LIKE '%$search%' OR Model LIKE '%$search%' OR Fuel_Type LIKE '%$search%'
+              OR Year LIKE '%$search%' OR Engine_Size LIKE '%$search%' OR Colour LIKE '%$search%'";
+              $results = mysqli_query($conn, $sql);
+              $queryResult = mysqli_num_rows($results);
+              if ($queryResult > 0) {
+                while ($row  = mysqli_fetch_assoc($results)) {
+                  include("cardisplayform.php");
+                }
+              } else {
+                echo 'no results match your search';
+              }
             }
-          } else {
-            // add query to database
-            $insert_sql = "INSERT INTO recentsearch (userID, searchQuery, amountSearched) VALUES ('$userID', '$search', '1');";
-            mysqli_query($conn, $insert_sql);
-          }
-          $sql = "SELECT * FROM Cars WHERE Make LIKE '%$search%' OR Model LIKE '%$search%' OR Fuel_Type LIKE '%$search%'
-          OR Year LIKE '%$search%' OR Engine_Size LIKE '%$search%' OR Colour LIKE '%$search%'";
-          $results = mysqli_query($conn, $sql);
-          $queryResult = mysqli_num_rows($results);
-          if ($queryResult > 0) {
-            while ($row  = mysqli_fetch_assoc($results)) {
-              include("cardisplayform.php");
+          } else if (isset($_POST['favourite-search'])) {
+            $search = "SELECT * FROM Favourites INNER JOIN Cars ON Favourites.carID = Cars.carID WHERE Favourites.userID = $userID";
+            $searchresult = mysqli_query($conn, $search);
+            $searchrow = mysqli_num_rows($searchresult);
+            if ($searchrow >= 1) {
+              while ($row = mysqli_fetch_assoc($searchresult)) {
+                include("cardisplayform.php");
+              }
+            } else {
+              echo '
+              <div>
+              <p>You currently have no favourited cars</p>
+              </div>
+              ';
             }
-          } else {
-            echo 'no results match your search';
           }
-        }
-      } else if (isset($_POST['favourite-search'])) {
-        $search = "SELECT * FROM Favourites INNER JOIN Cars ON Favourites.carID = Cars.carID WHERE Favourites.userID = $userID";
-        $searchresult = mysqli_query($conn, $search);
-        $searchrow = mysqli_num_rows($searchresult);
-        if ($searchrow >= 1) {
-          while ($row = mysqli_fetch_assoc($searchresult)) {
-            include("cardisplayform.php");
-          }
-        } else {
-          echo '
-          <div>
-          <p>You currently have no favourited cars</p>
-          </div>
-          ';
-        }
-      }
-      ?>
+          ?>
+        </div>
+      </div>
     </div>
   </div>
 
